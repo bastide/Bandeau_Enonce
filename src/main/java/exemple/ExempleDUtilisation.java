8efab51fe8fc63147625763c60aac2e36d4f84c4
Rémi Bastide <Remi.Bastide@irit.fr>
2024-12-04 13:47:39 +0100

    2 bandeaux

--- src/main/java/exemple/ExempleDUtilisation.java
+++ src/main/java/exemple/ExempleDUtilisation.java
@@ -12,50 +12,54 @@ public class ExempleDUtilisation {
     }
 
     public void exemple() {
-        Bandeau monBandeau = new Bandeau();
-        Font font = monBandeau.getFont();
-        Color back = monBandeau.getBackground();
-        Color fore = monBandeau.getForeground();
+        Bandeau bandeau1 = new Bandeau();
+        Bandeau bandeau2 = new Bandeau();
 
-        monBandeau.setMessage("Hello");
-        monBandeau.sleep(1000);
-        monBandeau.setMessage("On va changer de police");
-        monBandeau.sleep(1000);
-        monBandeau.setMessage("Monospaced 15 Bold");
-        monBandeau.setFont(new Font("Monospaced", Font.BOLD, 15));
-        monBandeau.sleep(1000);
-        monBandeau.setMessage("SansSerif 15");
-        monBandeau.setFont(new Font("SansSerif", Font.PLAIN, 15));
-        monBandeau.sleep(1000);
-        monBandeau.setMessage("On va zoomer");
-        monBandeau.sleep(1000);
-        monBandeau.setMessage("Zoom........");
+        Font font = bandeau1.getFont();
+        Color back = bandeau1.getBackground();
+        Color fore = bandeau1.getForeground();
+
+        bandeau1.setMessage("Hello");
+        bandeau1.sleep(1000);
+        bandeau1.setMessage("On va changer de police");
+        bandeau1.sleep(1000);
+        bandeau1.setMessage("Monospaced 15 Bold");
+        bandeau1.setFont(new Font("Monospaced", Font.BOLD, 15));
+        bandeau1.sleep(1000);
+        bandeau1.setMessage("SansSerif 15");
+        bandeau1.setFont(new Font("SansSerif", Font.PLAIN, 15));
+        bandeau1.sleep(1000);
+        // Bandeau 2
+        bandeau2.setMessage("On va zoomer");
+        bandeau2.sleep(1000);
+        bandeau2.setMessage("Zoom........");
         for (int i = 5; i < 60; i += 5) {
-            monBandeau.setFont(new Font("Dialog", Font.BOLD, 5 + i));
-            monBandeau.sleep(100);
+            bandeau2.setFont(new Font("Dialog", Font.BOLD, 5 + i));
+            bandeau2.sleep(100);
         }
-        monBandeau.sleep(1000);
+        bandeau2.sleep(1000);
+        // Bandeau 1
+        bandeau1.setFont(new Font("Courier new", Font.PLAIN, 15));
+        bandeau1.setMessage("On va tourner");
+        bandeau1.sleep(1000);
+        bandeau1.setMessage("On tourne de 45°...");
+        bandeau1.setRotation(Math.PI / 2.0f);
+        bandeau1.sleep(1000);
+        bandeau1.setRotation(0.0f);
 
-        monBandeau.setFont(new Font("Courier new", Font.PLAIN, 15));
-        monBandeau.setMessage("On va tourner");
-        monBandeau.sleep(1000);
-        monBandeau.setMessage("On tourne de 45°...");
-        monBandeau.setRotation(Math.PI / 2.0f);
-        monBandeau.sleep(1000);
-        monBandeau.setRotation(0.0f);
-
-        monBandeau.setMessage("On va changer de couleur de fond");
-        monBandeau.sleep(1000);
-        monBandeau.setBackground(Color.DARK_GRAY);
-        monBandeau.setMessage("On va changer de couleur");
-        monBandeau.sleep(1000);
-        monBandeau.setForeground(Color.YELLOW);
-        monBandeau.sleep(1000);
-        monBandeau.setFont(font);
-        monBandeau.setForeground(fore);
-        monBandeau.setBackground(back);
-        monBandeau.setMessage("Terminé");
-        monBandeau.sleep(3000);
-        monBandeau.close();
+        bandeau1.setMessage("On va changer de couleur de fond");
+        bandeau1.sleep(1000);
+        bandeau1.setBackground(Color.DARK_GRAY);
+        bandeau1.setMessage("On va changer de couleur");
+        bandeau1.sleep(1000);
+        bandeau1.setForeground(Color.YELLOW);
+        bandeau1.sleep(1000);
+        bandeau1.setFont(font);
+        bandeau1.setForeground(fore);
+        bandeau1.setBackground(back);
+        bandeau1.setMessage("Terminé");
+        bandeau1.sleep(3000);
+        bandeau1.close();
+        bandeau2.close();
     }
 }
